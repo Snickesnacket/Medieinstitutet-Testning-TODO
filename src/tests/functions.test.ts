3a0ce3526393c0abdadd54ffdd49f3492d10ef86
Astrid Lindfors <astrid.lindfors@gmail.com>
2023-03-15 20:12:29 +0100

    added tests for add toto and not add empty title

--- src/tests/functions.test.ts
+++ src/tests/functions.test.ts
@@ -1,22 +1,33 @@
 import { describe, it, expect } from 'vitest'
 import { addTodo,  toggleTodo, deleteTodo } from '../functions'
-import { Todo } from '../types/Todo'
+import { Todo } from '../types/Todo';
 
-describe('Add todo', () => {
-        const getResult: Todo[] = []
-        const title: string = "tjo"; 
-        const result = addTodo(title, getResult)
 
-    it('Should add a todo', () =>{
-        expect(result.success).toBe(true)
-        console.log("this is the result", result)
-        
-    })
+  
 
-     it.todo('Should not add a todo with empty title ', () => {
+describe('addTodo', () => {
+  it('should add a new todo to the list', () => {
+    const todos: Todo[] = []
+    const newTodoTitle = 'New todo'
+    const result = addTodo(newTodoTitle, todos) 
+
+     it('Should add a todo', () =>{
+    expect(result.success).toBe(true)
+    expect(todos).toHaveLength(1)
+    expect(todos[0].id).toBe(true)
+    expect(todos[0].title).toBe(newTodoTitle)
+    expect(todos[0].completed).toBeDefined
+
+     })
+})
+
+     it('Should not add a todo with empty title ', () => {
          expect(addTodo)
     })
-    it.todo('should not add a todo with title shorter than 3 characters', () =>{
+
+
+
+    it('should not add a todo with title shorter than 3 characters', () =>{
          expect(addTodo)
     })
 
@@ -40,3 +51,4 @@ describe.todo('Delete todo', () => {
          expect(deleteTodo)
     })
 })
+
